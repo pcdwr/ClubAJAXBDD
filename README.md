53700a6a340ed67445b9fee94da0f81dbe876073
David Robinson <davidr@pervasiveconcepts.com>
2014-05-05 16:43:15 -0500

    Updated README and test script

---
+++ README.md
@@ -0,0 +1,7 @@
+Add features and start testing
+
+1. Added features in tests/features
+2. Installed zombie
+3. Implemented rudimentary step definitions
+
+Next stop - addStepDefinitions
--- testFeatures.js
+++ testFeatures.js
@@ -1,7 +1,7 @@
 var exec = require('child_process').exec,
     child;
 
-child = exec('cucumber.js tests/features',
+child = exec('node_modules/.bin/cucumber.js tests/features -f pretty',
   function (error, stdout, stderr) {
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
